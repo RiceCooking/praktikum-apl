2f54eb361afa12659ff21b51ad18c23af734a800
Haekal <andihaekal338@gmail.com>
2026-02-27 06:25:12 +0800

    Struktur login dan awal menu utama

--- post-test/post-test-apl-1/2509106083-AndiMuhammadAlariceHaekal-PT-1.cpp
+++ post-test/post-test-apl-1/2509106083-AndiMuhammadAlariceHaekal-PT-1.cpp
@@ -2,7 +2,69 @@
 using namespace std;
 
 int main(){
+    int logout = 0;
+    bool menu;
+    string nama, password;
 
-    cout << "test";
+    while(logout != 3){
+        cout << "======================================" << endl;
+        cout << "===         Halaman Login          ===" << endl; 
+        cout << "======================================" << endl;
+        cout << "Username\t: ";cin >> nama;
+        cout << "Password\t: ";cin >> password;
+        if(nama == "Andi" && password == "083"){
+            cout << "Login berhasil" << endl;
+            cout << "======================================" << endl;
+            menu = true;
+            break;
+        }
+        else{
+            logout++;
+            if(nama != "Andi" && password == "083"){
+                cout << "Info\t\t: Nama tidak valid" << endl;
+            }
+
+            else if(password != "083" && nama == "Andi"){
+                cout << "Info\t\t: Password salah" << endl;
+            }
+
+            else{
+                cout << "Info\t\t: Username dan Password salah" << endl;
+            }
+            if(logout < 3){
+            cout << "Coba lagi";
+            for(int i = 4; i > 0; i--){
+                cout << ".";
+                for(long x = 0; x < 50000000; x++);
+            }
+            cout << endl << "======================================" << endl << endl << endl;
+            }
+        }
+
+        if(logout == 3){
+            cout << "Gagal login..." << endl;
+            cout << "======================================" << endl;
+        }
+        
+    }
+    cout << endl << endl;
+
+    while(menu == true){
+        string opsi;
+        cout << "======================================" << endl;
+        cout << "===         Menu Konversi          ===" << endl; 
+        cout << "======================================" << endl;
+        cout << "--------------------------------------" << endl;
+        cout << "|             Jenis Opsi             |" << endl;
+        cout << "|------------------------------------|" << endl;
+        cout << "|1. Meter -> Kilometer dan Centimeter|" << endl;
+        cout << "|2. Kilometer -> Meter dan Centimeter|" << endl;
+        cout << "|3. Centimeter -> Meter dan Kilometer|" << endl;
+        cout << "|4. Keluar                           |" << endl;
+        cout << "--------------------------------------" << endl;
+        cin >> opsi;
+        menu = false;
+    }
+    cout << "selesai";
     return 0;
 }
