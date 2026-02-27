2dba2db2290a3633f4f272c6e2cc7b485a867aa6
Haekal <andihaekal338@gmail.com>
2026-02-28 02:39:12 +0800

    kelas

---
+++ kelas/pertemuan-1/pertemuan-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "Hello, World! ";
+cout << "Belajar C + sangat seru😀";
+
+string nama = "Kei";
+int umur = 20;
+cout << "Halo, nama saya " << nama << "dan umur saya" << umur << "tahun" << endl;
+
+return 0;
+}
